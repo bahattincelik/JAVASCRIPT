084e7b2cef44c21e46d1e7b95d4858b55dd9bca9
Bahattin Celik <75189249+bahattincelik@users.noreply.github.com>
2021-05-24 02:39:51 +0300

    day05

--- bolum4_dom/app.js
+++ bolum4_dom/app.js
@@ -1,6 +1,13 @@
-const r = document.getElementById("r").value;
-// ! Class'lara erişim için .class_adı
-// ! id'li bir HTML elemanına erisim için ise #id
-const h = document.querySelector(".h").value;
+function hesapla() {
+  const r = document.getElementById("r").value;
+  // ! Class'lara erişim için .class_adı
+  // ! id'li bir HTML elemanına erisim için ise #id
+  const h = document.querySelector(".h").value;
 
-const hesapla = (r, h) => Math.PI * r * r * h;
+  const sonuc = (Math.PI * r * r * h).toFixed(2);
+  document.querySelector(".hacim").innerHTML = sonuc;
+}
+
+const buton = document.querySelector(".btn");
+buton.style.color = "red";
+buton.style.backgroundColor = "black";
