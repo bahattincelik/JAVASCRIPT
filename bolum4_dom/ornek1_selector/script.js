558dfc397b304179343791ce35c8f1d6200187d3
Bahattin Celik <75189249+bahattincelik@users.noreply.github.com>
2021-05-25 16:58:02 +0300

    day06

--- bolum4_dom/ornek1_selector/script.js
+++ bolum4_dom/ornek1_selector/script.js
@@ -18,4 +18,56 @@ buton.innerHTML = "ARA";
 //                   GETELEMENTSBYTAGNAME()
 // ======================================================
 
-const   
+const resim = document.getElementsByTagName("img");
+console.log(resim[0]);
+resim[0].style.width = "300px";
+resim[0].style.height = "'200px";
+
+resim[1].style.border = "3px solid red";
+
+// ======================================================
+//                   GETELEMENTSBYCLASSNAME()
+// ======================================================
+
+const h1 = document.getElementsByClassName("h1");
+h1[0].style.color = "red";
+h1[0].style.textAlign = "center";
+// ======================================================
+//                   QUERYSELECTOR()
+// ======================================================
+
+const baslik = document.querySelector(".title");
+baslik.innerHTML = "DOM SELECTOR 😎🐱‍🐉🎁";
+
+const body = document.querySelector("#body");
+body.style.backgroundImage = "linear-gradient(green, blue)";
+
+// ======================================================
+//                        EVENTS
+// ======================================================
+// ÖRNEK-1 (Mouse Over, Mouse Out )
+//-------------------------------------------------------
+const header1 = document.querySelector(".h1");
+const h1Renk = header1.style.backgroundColor;
+
+document.querySelector(".h1").onmouseover = function () {
+  const h1 = document.querySelector(".h1");
+  h1.style.color = "white";
+  h1.style.backgroundColor = "black";
+};
+
+document.querySelector(".h1").onmouseout = function () {
+  const h1 = document.querySelector(".h1");
+  h1.style.color = "red";
+  h1.style.backgroundColor = h1Renk;
+};
+
+// addEventListener()
+
+document.querySelector("#btn").addEventListener("mouseover", () => {
+  document.querySelector("#btn").style.width = "150px";
+});
+
+document.querySelector("#btn").addEventListener("mouseout", function () {
+  document.querySelector("#btn").style.width = "100px";
+});
