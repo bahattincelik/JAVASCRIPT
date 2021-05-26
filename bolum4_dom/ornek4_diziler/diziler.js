c0b0f899a7dc7a397beb37799d255dfccdc9e455
Bahattin Celik <75189249+bahattincelik@users.noreply.github.com>
2021-05-26 22:35:16 +0300

    day08

--- bolum4_dom/ornek4_diziler/diziler.js
+++ bolum4_dom/ornek4_diziler/diziler.js
@@ -25,7 +25,7 @@ console.log(isimler);
 // ========= DİZİYİ DEĞİŞTİREN METOTLAR ==============
 const h1 = document.getElementById("meyveler");
 let meyveler = ["Elma", "Armut", "Muz", "Kivi"];
-// pop() son elemani siler..
+// pop() son elemani siler ve silenen elemani dondurur.
 meyveler.pop(); // ["Elma", "Armut", "Muz"];
 // push() dizinin sonuna eleman ekledi.
 // dizinin son eleman sayisini dondurur.
@@ -36,6 +36,23 @@ const boyut1 = meyveler.unshift("ayva");
 console.log(boyut1);
 // dizinin 0. indeks elemanini siler  ve silenen elemani dondurur.
 const silinen = meyveler.shift();
+console.log(meyveler);
+
+//Dizinin tamamini terse cevirir
+meyveler.reverse();
+
+meyveler.push("Kavun");
+meyveler.sort();
+meyveler.splice(1, 0, "Erik");
+
+meyveler.splice(4, 1, "Mango");
+
 console.log(silinen);
 console.log(meyveler);
 h1.innerHTML = meyveler;
+
+// ========= DİZİYİ ERISIM METOTLAR ==============
+
+const sayilar = [3, 5, 2, "2", "uc", 2, "bes", 5];
+
+console.log(sayilar.includes(5));
