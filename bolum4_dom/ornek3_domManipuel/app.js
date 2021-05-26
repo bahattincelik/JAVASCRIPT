9ef076c11d94bc0cf74b10446ed06931aea3853b
Bahattin Celik <75189249+bahattincelik@users.noreply.github.com>
2021-05-26 20:49:33 +0300

    day08

--- bolum4_dom/ornek3_domManipuel/app.js
+++ bolum4_dom/ornek3_domManipuel/app.js
@@ -34,8 +34,29 @@ const yazi = document.createTextNode("Programlama Dilleri");
 h1.appendChild(yazi);
 // sonDiv.appendChild(h1);
 sonDiv.after(h1);
-h1.style.marginTop = "20px";
-h1.style.color = "red";
-h1.style.textAlign = "left";
+h1.className = "yeni-h1";
+//Olusturdugumuz h1 elemanina bir class adi atadik.
+// h1.style.marginTop = "20px";
+// h1.style.color = "red";
+// h1.style.textAlign = "left";
 
-document.querySelector(".ekle").onclick = function () {};
+document.querySelector(".ekle").onclick = function () {
+  // ekleme yapılacak listeyi al
+  const liste = document.querySelector(".liste");
+  //input elemanina girilen yeni satirin degerini al
+  const satir = document.querySelector(".dil").value;
+  // yeni girilen satiri saklamak icin bir li olusturduk.
+  const yeniLi = document.createElement("li");
+  // yeni li icin textnode olusturduk
+  const textNode = document.createTextNode(satir);
+  //olusturdugumuz texnode'u yeni li'ye bagladik.
+  yeniLi.appendChild(textNode);
+  // yeni eklenen satiri var olan listeye (ul) baglayalim.
+  liste.appendChild(yeniLi);
+};
+
+document.querySelector(".sil").onclick = function () {
+  const liste = document.querySelector(".liste");
+
+  liste.removeChild(liste.lastElementChild);
+};
