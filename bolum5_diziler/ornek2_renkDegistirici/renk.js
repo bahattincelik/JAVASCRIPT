55949d3131f3c0bfd3d4887afe85cb187245da0a
Bahattin Celik <75189249+bahattincelik@users.noreply.github.com>
2021-05-27 15:30:58 +0300

    day09

---
+++ bolum5_diziler/ornek2_renkDegistirici/renk.js
@@ -0,0 +1,32 @@
+const renkler = [
+  "red",
+  "blue",
+  "green",
+  "yellow",
+  "black",
+  "pink",
+  "purple",
+  "brown",
+];
+
+const rastgeleSayi = Math.floor(Math.random() * renkler.length);
+
+document.querySelector("body").style.backgroundColor = renkler[rastgeleSayi];
+
+document.querySelector(".btn").addEventListener("click", () => {
+  const renkler = [
+    "red",
+    "blue",
+    "green",
+    "yellow",
+    "black",
+    "pink",
+    "purple",
+    "brown",
+  ];
+
+  const rastgeleSayi = Math.floor(Math.random() * renkler.length);
+
+  document.querySelector("body").style.backgroundColor = renkler[rastgeleSayi];
+  document.querySelector(".renk").innerHTML = renkler[rastgeleSayi];
+});
