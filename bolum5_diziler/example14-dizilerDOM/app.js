02e3ea9e81bcf634b40d3aa2ce560091ced4cdae
Bahattin Celik <75189249+bahattincelik@users.noreply.github.com>
2021-05-29 23:53:35 +0300

    day11

--- bolum5_diziler/example14-dizilerDOM/app.js
+++ bolum5_diziler/example14-dizilerDOM/app.js
@@ -1,2 +1,43 @@
-const maaslar = [4000, 5000, 3500, 4200, 2850];
+const maaslar = [4000, 5000, 3500, 4200, 2850, 5000, 7500];
 
+const liste = document.createElement("ul");
+liste.className = "liste";
+document.querySelector(".liste-div").appendChild(liste);
+
+const listeyeEkle = function (satir) {
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(satir));
+  liste.appendChild(li);
+};
+
+const diziyiListeyeEkle = function () {
+  for (let i in maaslar) {
+    listeyeEkle(maaslar[i]);
+  }
+};
+
+diziyiListeyeEkle();
+
+document.querySelector(".ekle").onclick = function () {
+  const satir = document.querySelector(".input-liste");
+  if (!satir.value) {
+    alert("Lutfen veri girisi yapiniz");
+  } else {
+    //inputtan gelen veriyi diziye kaydet
+    maaslar.push(Number(satir.value));
+    //inputten gelen gelen veriyi listeye yazdir
+    listeyeEkle(satir.value);
+    //islemler bittikten sonra inputta kalan veriyi sil
+    satir.value = "";
+  }
+};
+
+document.querySelector(".sil").onclick = function () {
+  if (maaslar.length == 0) {
+    alert("Silinecek oge kalmadi");
+  } else {
+    maaslar.pop();
+    const liste = document.querySelector(".liste");
+    liste.removeChild(liste.lastElementChild);
+  }
+};
