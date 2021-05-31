899c0bda3acbb0bf0338388c3f01136488c598b5
Bahattin Celik <75189249+bahattincelik@users.noreply.github.com>
2021-05-31 22:47:09 +0300

    day12

--- bolum6_objeler/nesneler.js
+++ bolum6_objeler/nesneler.js
@@ -43,3 +43,85 @@ insan.konum = "Turkiye";
 insan.email = "a@gmail.com";
 insan["dogumTarihi"] = 1990;
 console.log(insan);
+
+// ======================================================
+//            NESNE METODLARI
+//======================================================
+
+const kisi = {
+  ad: "Ahmet",
+  soyad: "Can",
+  dogumTarihi: 1990,
+  meslek: "tester",
+  tool: "selenium",
+  ehliyet: true,
+  yasHesapla: function () {
+    console.log(this);
+    return new Date().getFullYear() - this.dogumTarihi;
+  },
+  ozet: function () {
+    return `${this.ad} ${this.soyad} ${this.yasHesapla()} yasindadir. Mesleği ${
+      this.meslek
+    } likdir.`;
+  },
+};
+const tarih = new Date();
+console.log(tarih.getTime());
+console.log(tarih.getDay());
+console.log(kisi.yasHesapla());
+console.log(kisi.ozet());
+
+// ======================================================
+//                  Nesne Iterasyon Ornekleri
+// ======================================================
+const kisiler = [
+  {
+    ad: "mustafa",
+    soyad: "can",
+    meslek: "developer",
+    yas: 20,
+  },
+  {
+    ad: "ayse",
+    soyad: "yilmaz",
+    meslek: "tester",
+    yas: 25,
+  },
+  {
+    ad: "canan",
+    soyad: "ari",
+    meslek: "grafiker",
+    yas: 35,
+  },
+  {
+    ad: "ali",
+    soyad: "veli",
+    meslek: "team lead",
+    yas: 26,
+  },
+  {
+    ad: "ceren",
+    soyad: "yilmaz",
+    meslek: "developer",
+    yas: 24,
+  },
+];
+console.log(kisiler);
+// ÖRNEK: kisiler dizisindeki kisilerin mesleklerini konsola yazdiralim.
+kisiler.forEach((x) => console.log(x.meslek));
+//ÖRNEK:kisiler dizisindeki tüm bireylerin yaşını bir arttırarak yazdırınız.
+const yaslar = kisiler.map((x) => x.yas + 1);
+console.log(yaslar);
+// kisiler dizisinindeki kişilerin isimlerini büyük harf olarak değiştiren ve yaşlarini da 5 artırak yeni bir nesne oluşturan kodu yaziniz.
+const buyukHarf = kisiler.map((x) => {
+  return {
+    ad: x.ad.toUpperCase(),
+    yas: x.yas + 5,
+  };
+});
+console.log(buyukHarf);
+// Yaşı 25' eşit veya küçük olanların adlarını yazdıran kodu yazınız.
+
+// Mesleği developer olanların isim ve yaşlarını bir diziye kaydediniz.
+
+// kisilerin ortalama yasini hesaplayiniz.
